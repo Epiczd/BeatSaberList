b2f955e78ee445a98b2023218e680ab7cebde92c
Epiczd <nutball37@gmail.com>
2020-02-13 13:53:28 -0500

    Can add players

--- BeatSaberList/Program.cs
+++ BeatSaberList/Program.cs
@@ -6,7 +6,13 @@ namespace BeatSaberList
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] players = new string[5];
+            Console.WriteLine("Please Enter The Players Playing Today");
+            players[0] = Console.ReadLine();
+            Console.WriteLine($"Player 1 is {players[0]}...");
+            Console.WriteLine("Any more players?");
+            Console.Read();
         }
+            
     }
 }
