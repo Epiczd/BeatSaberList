a11b439de70b1eadd806ba5a921a24666323bb1a
Epiczd <nutball37@gmail.com>
2020-02-13 15:13:31 -0500

    Done with the main application

--- BeatSaberList/Program.cs
+++ BeatSaberList/Program.cs
@@ -6,21 +6,27 @@ namespace BeatSaberList
     {
         static void Main(string[] args)
         {
-            string[] players = new string[25];
             string morePlayers;
             string list;
             int playerCount = 0;
             string finished;
             int nextPlayer = 0;
 
+            Console.WriteLine("Please Enter The Number of Players Playing Today");
+            int numOfPlayers = Convert.ToInt32(Console.ReadLine());
+            string[] players = new string[numOfPlayers];
+            Console.WriteLine($"{numOfPlayers} are playing today!");
+            Console.WriteLine("\n");
             Console.WriteLine("Please Enter The Players Playing Today");
             players[0] = Console.ReadLine();
             playerCount++;
+            Console.WriteLine("\n");
             Console.WriteLine($"Player 1 is {players[0]}...");
             AnyMorePlayers();
 
             void AnyMorePlayers()
             {
+                Console.WriteLine("\n");
                 Console.WriteLine("Any more players?");
                 morePlayers = Console.ReadLine();
 
@@ -41,9 +47,11 @@ namespace BeatSaberList
 
             void Yes()
             {
+                Console.WriteLine("\n");
                 Console.WriteLine("Please Enter The Players Playing Today");
                 players[playerCount] = Console.ReadLine();
                 playerCount++;
+                Console.WriteLine("\n");
                 Console.WriteLine($"Player {playerCount} is {players[playerCount - 1]}...");
                 AnyMorePlayers();
                 
@@ -51,6 +59,7 @@ namespace BeatSaberList
 
             void No()
             {
+                Console.WriteLine("\n");
                 Console.WriteLine("Do You Want To See A List Of Players?");
                 list = Console.ReadLine();
 
@@ -73,7 +82,17 @@ namespace BeatSaberList
 
             void Turn()
             {
-                Console.WriteLine($"Player {players[nextPlayer]} is up!");
+                Console.WriteLine("\n");
+
+                Console.WriteLine($"Player {players[nextPlayer]} is up!, Good Luck Gamer");
+                Console.WriteLine("\n");
+
+                if (nextPlayer == 0)
+                {
+                    Console.WriteLine("[WARNING]: Any Answer Other Than Yes (with a capital Y) will end the program!");
+                    Console.WriteLine("\n");
+                }
+
                 Console.WriteLine("Is This Player Finished?");
                 finished = Console.ReadLine();
 
@@ -81,8 +100,15 @@ namespace BeatSaberList
                 {
                     case "Yes":
                         {
-                            nextPlayer++;
-                            Turn();
+                            if(nextPlayer < numOfPlayers - 1)
+                            {
+                                nextPlayer++;
+                                Turn();
+                            }
+                            else
+                            {
+                                EndOfProgram();
+                            }
                         }
                         break;
                 }
